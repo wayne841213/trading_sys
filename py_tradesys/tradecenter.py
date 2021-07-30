4b9a636b7a5c9be143212825621ce46ead5a08ec
williamchou1013 <drizzle60902@gmail.com>
2021-07-30 09:45:59 +0800

    design the grab_historical_prices func

--- py_tradesys/tradecenter.py
+++ py_tradesys/tradecenter.py
@@ -1,13 +1,15 @@
 # API part
 from datetime import datetime, time, timezone
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Union
 
 import pandas as pd
 from td.client import TDClient
+from td.utils import TDUtilities
 
+from asset_class import StockFrame
 from portfolio import Portfolio
 
-# from td.utils import milliseconds_since_epoch  # issue to be solve
+milliseconds_since_epoch = TDUtilities().milliseconds_since_epoch
 
 
 # paper trading = mock trading
@@ -118,9 +120,60 @@ class Tradecenter:
 
         return quotes
 
-    def grab_historical_prices(self) -> List[Dict]:
-        pass
+    def create_stock_frame(self, data: List[dict]) -> StockFrame:
+        self.stock_frame = StockFrame(data=data)
+        return self.stock_frame
 
-    def create_stock_frame(self):
-        pass
+    # important
+    def grab_historical_prices(
+        self,
+        start: datetime,
+        end: datetime,
+        bar_size: int = 1,
+        bar_type: str = "minute",
+        symbol: Optional[List[str]] = None,
+    ) -> List[Dict]:
+
+        self.bar_size = bar_size
+        self.bar_type = bar_type
+
+        start = str(milliseconds_since_epoch(dt_object=start))
+        end = str(milliseconds_since_epoch(dt_object=end))
+
+        new_prices = []
+
+        if not symbols:
+            symbols = self.portfolio.positions
+
+        for symbol in symbols:
+
+            historical_prices_response = self.session.get_price_history(
+                symbol=symbol,
+                period_type="day",
+                start_date=start,
+                end_date=end,
+                frequency_type=bar_type,
+                frequency=bar_size,
+                extended_hours=True,
+            )
+
+            self.historical_prices[symbol] = {}
+            self.historical_prices[symbol]["candles"] = historical_prices_response["candles"]
+
+            for candle in historical_prices_response["candles"]:
+
+                new_price_mini_dict = {}
+                new_price_mini_dict["symbol"] = candle["symbol"]
+                new_price_mini_dict["open"] = candle["open"]
+                new_price_mini_dict["high"] = candle["high"]
+                new_price_mini_dict["low"] = candle["low"]
+                new_price_mini_dict["close"] = candle["close"]
+                new_price_mini_dict["volume"] = candle["volume"]
+                new_price_mini_dict["datetime"] = candle["datetime"]
+
+                new_prices.append(new_price_mini_dict)
+
+        self.historical_prices["aggregated"] = new_prices
+
+        return self.historical_prices
 
